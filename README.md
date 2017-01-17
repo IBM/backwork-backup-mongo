bf7d8b38ec53262cd4ce7af9e1ae284f98a94802
Luiz Aoqui <laoqui@ca.ibm.com>
2017-01-17 00:01:45 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#monsoon-backup-mongo
---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md')) as f:
+    long_description = f.read()
+
+setup(
+    name="monsoon-backup-mongo",
+    version="0.1.0",
+    description="Monsoon plug-in for MongoDB backups.",
+    long_description=long_description,
+    url="https://github.ibm.com/apset/monsoon",
+    author="Luiz Aoqui",
+    author_email="laoqui@ca.ibm.com",
+    license="IBM",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 2 :: Only",
+        "Topic :: Database",
+        "Topic :: System :: Archiving :: Backup",
+        "Topic :: Utilities"
+    ],
+    packages=find_packages(),
+    install_requires=["monsoon"],
+    entry_points={
+        "monsoon.backups": [
+            "mongo=mongo:MongoBackup"
+        ]
+    }
+)
